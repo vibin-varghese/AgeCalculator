78a4d81289221eceab94e9107b5cee30af6080f0
Vibin Varghese <vvarghe2@partner.jaguarlandrover.com>
2018-03-19 14:00:38 +0000

    Added new methods findAge1 and findAge2

--- AgeCalculator/src/com/org/jlr/age/AgeCalculator.java
+++ AgeCalculator/src/com/org/jlr/age/AgeCalculator.java
@@ -1,14 +1,18 @@
 package com.org.jlr.age;
 
 public class AgeCalculator {
-	
+
 	int i = 0;
 
 	public void findAge(String age) {
-		
+
 	}
-	
+
 	public void findAge1(String age) {
-		
+
+	}
+
+	public void findAge2(String age) {
+
 	}
 }
